786368bab64fa8938f2f41fa2d4a46fa57797207
Robert Clark <rbclark@mitre.org>
2014-07-01 17:33:44 -0400

    Add support for automatically generating SSL certificates.

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,6 +8,7 @@ default.nginx_passenger.ruby                = "/usr/bin/ruby"
 default.nginx_passenger.max_pool_size       = 8
 
 default.nginx_passenger.cert_databag        = "ssl_certs"
+default.nginx_passenger.cert_authority      = "Self Signed"
 
 default.nginx_passenger.redirect_to_https   = true
 default.nginx_passenger.site_min_instances  = 2
