a46d7d1dacbe8a73b8d0c9e06f111e564fb1483b
Lucas Souza <lucasas@gmail.com>
2016-03-24 13:47:20 -0300

    Unfortunately chef 11 does not support source_url

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,6 @@ name             "nginx_passenger"
 maintainer       "Eric Richardson"
 maintainer_email "e@ewr.is"
 license          "BSD"
-source_url       "https://github.com/ewr/nginx_passenger-cookbook"
-issues_url       "https://github.com/ewr/nginx_passenger-cookbook/issues"
 description      "Installs/Configures nginx and Passenger on Ubuntu"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.5.7"
